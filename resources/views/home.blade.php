4b2f3ecb4890064fc87cf4cdd4eb02cd0da722e9
K. Wirchanowicz <kondziori19@gmail.com>
2022-09-01 18:38:45 +0200

    Add Create Post Button, Change img, iframe size to fit in article content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+img{
+object-fit: cover;
+width: 100% !important;
+height: 500px !important;
+}
+iframe{
+    object-fit: fill;
+    display:block;
+    width:100% !important;
+    height: 500px !important;
+}
+</style>
 <div class="container">
     <div class="row justify-content-center gx-5">
-        <div class="col-8">     
+        <div class="col-8">
+            <div style="max-height: inherit; max-width:inherit">
             @foreach ($articles as $article)
                 @include('components.article')
-            @endforeach 
+            @endforeach
+            </div>
         </div>
         <div class="col-4">
-            <div class="border rounded"> 
+            <a href='{{route('article')}}' class="btn btn-success btn-lg" style='width:100%'>Create Post</a>
+            <div class="border rounded my-5">
                 test2
             </div>
         </div>
